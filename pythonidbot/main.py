e383e333332509bd5de3ab0e1d4b8285194a5e0e
hexatester <revolusi147id@gmail.com>
2021-03-20 17:03:55 +0700

    Add DISABLED_LOGGER

--- pythonidbot/main.py
+++ pythonidbot/main.py
@@ -2,15 +2,21 @@ import logging
 import sys
 
 from telegram.ext import Updater
-from pythonidbot.config import BOT_TOKEN, PORT, APP_URL
+from typing import List, Optional
+
+from pythonidbot.config import BOT_TOKEN, PORT, APP_URL, DISABLED_LOGGER
 from pythonidbot.handlers import register
 
 
-def set_logging(debug: bool = False):
+def set_logging(debug: bool = False, disabled: Optional[List[str]] = None):
     logging.basicConfig(
         level=logging.DEBUG if debug else logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
+    disabled = disabled or DISABLED_LOGGER
+    for name in disabled:
+        _logger = logging.getLogger(name)
+        _logger.setLevel(logging.INFO)
 
 
 def main():
