d237d96b9b356535af548d86d5484fb0f9b88d6f
hexatester <revolusi147id@gmail.com>
2021-03-21 12:38:16 +0700

    Move HINTS to .constants

--- pythonidbot/taghints.py
+++ pythonidbot/taghints.py
@@ -5,64 +5,8 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackContext
 
+from pythonidbot.constants import HINTS
 from pythonidbot.utils.helpers import build_menu
-
-HINTS = {
-    "#aturan": {
-        "message": "{query} Silahkan baca Aturan Grup Telegram PythonID di https://pythonid.github.io/aturan.html",
-        "default": "Hai.",
-        "help": "Kirim peraturan group",
-    },
-    "#private": {
-        "message": "Tolong jangan spam group dengan {query}, dan sebaiknya "
-        "obrolan pribadi dengan saya. Terimakasih, anggota lain akan menghargainya 😊",
-        "default": "pencarian atau perintah",
-        "buttons": [
-            {
-                "text": "🤖 Ke obrolan pribadi",
-                "url": "https://t.me/pitonidbot",
-            }
-        ],
-        "help": "Beri tahu anggota untuk berhenti mengirim spam dan beralih ke obrolan pribadi",
-    },
-    "#meta": {
-        "message": """No need for meta questions. Just ask! 🤗
-<i>"Has anyone done .. before?"</i>
-Probably. <b>Just ask your question and somebody will help!</b> 
-        """,
-        "help": "Show our stance on meta-questions",
-    },
-    "#belajar": {
-        "message": "{query}\nKami sudah mengumpulkan rekomendasi sumber belajar di https://pythonid.github.io/belajar.html",
-        "help": "Rekomendasi Anggota Untuk Mulai Belajar Python",
-        "default": "Oh, Hei! Rupanya ada yang ingin bergabung dengan komunitas pengembang python ❤️ ",
-    },
-    "#bertanya": {
-        "message": "{query} Agar kami tidak segan menjawab pertanyaan anda silahkan mohon dibaca Bertanya Yang Baik di https://pythonid.github.io/bertanya.html",
-        "help": "Bagaimana Bertanya Yang Baik",
-        "default": "Hei.",
-    },
-    "#pastebin": {
-        "message": """{query} Harap poskan kode menggunakan pastebin, bukan sebagai teks biasa atau screenshot. https://pastebin.com/ adalah yang paling populer, tetapi ada banyak alternatif di luar sana. Tentu saja, untuk potongan yang sangat pendek, teks sudah cukup. Harap setidaknya memformatnya sebagai monospace. Terimakasih""",
-        "help": "Beri tahu pengguna agar tidak mengirim kode sebagai teks atau gambar.",
-        "default": "Hei.",
-    },
-    "#postganda": {
-        "message": """{query} Please don't double post. Questions usually are on-topic only in one of the two groups anyway.""",
-        "help": "Beri tahu pengguna agar tidak mengirim pertanyaan yang sama berkali-kali",
-        "default": "Hey.",
-    },
-    "#xy": {
-        "message": """{query} Masalah XY menanyakan tentang solusi yang Anda coba daripada masalah Anda yang sebenarnya. Hal ini menyebabkan pemborosan waktu dan energi yang sangat besar, baik di pihak orang yang meminta bantuan, maupun di pihak mereka yang memberikan bantuan..""",
-        "default": "Hei. Untuk apa sebenarnya Anda menginginkan ini?",
-        "help": "Tanyakan kepada pengguna tentang kasus penggunaan yang sebenarnya.",
-    },
-    "#janganping": {
-        "message": """{query} Harap hanya sebutkan atau balas pengguna secara langsung jika Anda menindaklanjuti percakapan dengan mereka. Jika tidak, ajukan pertanyaan Anda dan tunggu apakah seseorang memiliki solusi untuk Anda - begitulah cara kerja grup ini 😉 Perhatikan juga bahwa tag <code> @admin </code> hanya digunakan untuk melaporkan spam atau penyalahgunaan!""",
-        "default": "Hei.",
-        "help": "Beri tahu pengguna untuk tidak melakukan ping secara acak ke Anda.",
-    },
-}
 
 
 def list_available_hints(update: Update, context: CallbackContext):
