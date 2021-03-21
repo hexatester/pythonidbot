8ca70e74cfb9bf3bfba07124e050a67aa3b64ae9
hexatester <revolusi147id@gmail.com>
2021-03-21 17:14:20 +0700

    Initial inline_handler

--- pythonidbot/inline/__init__.py
+++ pythonidbot/inline/__init__.py
@@ -1,4 +1,25 @@
+from telegram import Update
+from telegram.ext import CallbackContext, InlineQueryHandler
+from .hints import Hints
+
+
+HINTS = Hints()
+
+
+def inline_handler(update: Update, context: CallbackContext):
+    if not update.inline_query or not update.inline_query.query:
+        return
+    if len(update.inline_query.query) < 3:
+        return update.inline_query.answer(HINTS.hints_article)
+    articles = list()
+    articles.extend(Hints.find(update.inline_query.query))
+    if not articles:
+        articles.extend(HINTS.hints_article)
+    return update.inline_query.answer(articles)
+
+
 HANDLERS = []
+HANDLERS.append(InlineQueryHandler(inline_handler))
 
 
 def register_inlines(dispatcher):
