a340316ffe2838ac04c9bd3bb5f79c5eba22bddb
hexatester <revolusi147id@gmail.com>
2021-03-21 17:28:29 +0700

    register_inlines

--- pythonidbot/handlers.py
+++ pythonidbot/handlers.py
@@ -4,12 +4,14 @@ from pythonidbot.callback import register_callbacks
 from pythonidbot.command import register_commands
 from pythonidbot.conversation import register_conversations
 from pythonidbot.error import register_errors
+from pythonidbot.inline import register_inlines
 
 REGISTERS = [
     register_errors,
     register_conversations,
     register_callbacks,
     register_commands,
+    register_inlines,
 ]
 
 
