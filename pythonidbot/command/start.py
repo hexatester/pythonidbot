1a7a90920993009886638b72fd436532629003e2
hexatester <revolusi147id@gmail.com>
2021-03-22 09:36:49 +0700

    Update start

--- pythonidbot/command/start.py
+++ pythonidbot/command/start.py
@@ -4,9 +4,11 @@ from telegram.ext import CallbackContext, CommandHandler
 
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
-    update.message.reply_text(
-        "Hai. Aku bot yang mengumumkan aturan di python indonesia"
-    )
+    if update.effective_user:
+        name = update.effective_user.full_name
+        update.message.reply_text(f"Selamat datang {name}!")
+    else:
+        update.message.reply_text(f"Hai!")
 
 
 START_HANDLERS = [CommandHandler("start", start)]
