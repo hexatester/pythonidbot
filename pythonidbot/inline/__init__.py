79008ab534885029f1a16410de5c24b58b0b9668
hexatester <revolusi147id@gmail.com>
2021-03-23 13:18:25 +0700

    Refactor inline_handler

--- pythonidbot/inline/__init__.py
+++ pythonidbot/inline/__init__.py
@@ -12,17 +12,24 @@ def inline_handler(update: Update, context: CallbackContext):
     inline_query = update.inline_query
     articles = list()
     if len(inline_query.query) < 4:
-        return inline_query.answer(HINTS.hints_article)
-    if inline_query.query.startswith("#"):
-        return HINTS.hashtag(inline_query)
+        return inline_query.answer(
+            HINTS.hints_article,
+            switch_pm_parameter="inline-help",
+            switch_pm_text="Bantuan",
+        )
     articles.extend(HINTS.find(inline_query.query))
     if not articles:
         articles.extend(HINTS.hints_article)
-    inline_query.answer(articles)
+    inline_query.answer(
+        articles,
+        switch_pm_parameter="inline-help",
+        switch_pm_text="Bantuan",
+    )
     return
 
 
 HANDLERS = []
+HANDLERS.append(InlineQueryHandler(HINTS.hashtag_handler, pattern=r"^#\S+"))
 HANDLERS.append(InlineQueryHandler(inline_handler))
 
 
