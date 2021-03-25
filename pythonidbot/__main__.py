41b81fb025b3054207d1f28cc74254cc023a36f5
hexatester <revolusi147id@gmail.com>
2021-03-25 18:15:45 +0700

    Move load_dotenv to .config

--- pythonidbot/__main__.py
+++ pythonidbot/__main__.py
@@ -1,14 +1,4 @@
 import sys
-
-try:
-    from dotenv import load_dotenv
-
-    load_dotenv()
-except ImportError:
-    print("Please install python-dotenv (pip install python-dotenv).")
-    print("Then create .env file inside root project folder (like .gitignore),")
-    print("Add line `BOT_TOKEN=<token from @botfather>` to .env")
-
 from .main import main
 
 
--- pythonidbot/config.py
+++ pythonidbot/config.py
@@ -1,5 +1,16 @@
 import os
 
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    if "BOT_TOKEN" not in os.environ:
+        print("Please install python-dotenv (pip install python-dotenv).")
+        print("Then create .env file inside root project folder (like .gitignore),")
+        print("Add line `BOT_TOKEN=<token from @botfather>` to .env")
+
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PORT = os.environ.get("PORT")
 APP_URL = os.environ.get("APP_URL")
